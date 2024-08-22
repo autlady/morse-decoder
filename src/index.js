b3070fc9ff835606a2be0fce3b422ff31c2cb83f
Julia Grigoreva <julia_panina@mail.ru>
2024-08-23 00:59:18 +0300

    fix: func decode from string to morse

--- src/index.js
+++ src/index.js
@@ -39,8 +39,16 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     let res = "";
-    res = Object.keys(MORSE_TABLE).find(key =>
-        MORSE_TABLE[key] === expr);
+    let array = Array.from(expr);
+    console.log(array);
+    for (let index = 0; index < array.length; ++index) {
+        if (array[index] === " ") {
+            res = res + "******";
+        } else {
+            res = res + Object.keys(MORSE_TABLE).find(key =>
+                MORSE_TABLE[key] === array[index]);
+        } 
+    }
     return res;
 }
 
